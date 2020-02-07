a5b186ec3b5fce8d7497fc498fb651ac434258d8
Oliver Inglis <ollie@theinglistwins.com>
2020-02-07 18:23:15 +0000

    working image list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,14 +12,20 @@ class App extends Component {
       params: { query: term }
     }).then((response) => {
       this.setState({ images: response.data.results });
+      console.log(response.data.results);
     });
   }
 
   render () {
+    const style = {
+      width: '200px'
+    }
     return (
       <div className="ui container margin-top">
         <SearchBar onMySubmit={this.onSearchSubmit} />
+        <ImageList useImages={this.state.images} />
         <p>Found: { this.state.images.length } images</p>
+
       </div>
     );
   }
--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 class ImageList extends Component {
-  state = { images: ['https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg'] }
   render() {
+    const style = {
+      width: '200px'
+    };
     return (
-
-<img src={image}> </img>
-
-    )
+      <div>
+        {this.props.useImages.map(image => {
+          return <img key={image.id} src={image.urls.regular} alt="" style={style} />
+        })}
+      </div>
+    );
   }
 }
 
